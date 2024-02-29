01d23d0e8db089605b900a61d434cd95e6d42982
xuliangs <xu94liang@gmail.com>
2024-02-29 21:43:28 +0000

    sdf action init commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-# Container image that runs your code
-FROM alpine:3.10
+# Debian bookworm uses 2.26 glibc where 2.35 is required to run sdf
+FROM rust:1.76.0-bookworm
+ARG SDF_VERSION=0.1.169
+
+# Install sdf
+RUN curl -LSfs https://cdn.sdf.com/releases/download/install.sh | bash -s -- --version ${SDF_VERSION}
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/sh -l
 
-echo "Hello $1!"
-time=$(date)
-echo "time=$time" >>$GITHUB_OUTPUT
+set -eo pipefail
+
+echo "workspace dir set as: \"${WORKSPACE_DIR}\""
+cd ${WORKSPACE_DIR}
+
+echo "run 'sdf auth login' using headless credentials"
+sdf auth login --access-key "${ACCESS_KEY}" --secret-key "${SECRET_KEY}"
+
+LOG_FILE="output.${GITHUB_RUN_ID}.txt"
+LOG_PATH="${WORKSPACE_DIR}/${LOG_FILE}"
+echo "LOG_PATH=${LOG_PATH}" >>$GITHUB_ENV
+echo "saving output in \"${LOG_PATH}\""
+$1 2>&1 | tee "${LOG_FILE}"
+if [ $? -eq 0 ]; then
+  echo "RUN_STATE=passed" >>$GITHUB_ENV
+  echo "result=passed" >>$GITHUB_OUTPUT
+  echo "SDF run OK" >>"${LOG_FILE}"
+else
+  echo "RUN_STATE=failed" >>$GITHUB_ENV
+  echo "result=failed" >>$GITHUB_OUTPUT
+  echo "SDF run failed" >>"${LOG_FILE}"
+  exit 1
+fi
